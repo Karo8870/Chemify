2fb3841d3304297a34ee651f31bbc19b1933aeb1
karo <cazacuchristian@gmail.com>
2025-01-28 06:23:13 +0000

    Added table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { IonApp, IonButton, IonRow } from '@ionic/angular/standalone';
 import { ThemeButtonComponent } from '../components/theme-button/theme-button.component';
+import { NgStyle } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrl: 'app.component.scss',
-  imports: [IonApp, IonRow, IonButton, ThemeButtonComponent]
+  imports: [IonApp, IonRow, IonButton, ThemeButtonComponent, NgStyle]
 })
 export class AppComponent {
 }
